93663b0a125b81783fc1b9d54f9be9904801b3ed
saya3838 <86864442+saya3838@users.noreply.github.com>
2022-03-09 23:04:23 +0900

    日にちの変更

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
 import datetime
-birth_day = datetime.date(1983, 11, 3)
+birth_day = datetime.date(1999, 1, 1)
 today = datetime.date.today()
 print('私が生まれてから'+str(today - birth_day)[:-14]+'日経ちました。')
